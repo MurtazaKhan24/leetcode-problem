24d4c56008e3b8fb0bc837b80f6a05d3fcc2f3af
MurtazaKhan24 <khanmurtaza001@gmail.com>
2024-07-13 13:12:16 -0500

    Added to Binary Search folder

---
+++ 05 - Binary Search/02 - 2DBinarySearch.py
@@ -0,0 +1,17 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        m = 0
+        while m < len(matrix):
+            l,r = 0, len(matrix[m])
+            if target >= matrix[m][0] and matrix[m][-1] >= target: 
+                while l < r:
+                    mid = (l + r) // 2
+                    if matrix[m][mid] < target:
+                        l += 1
+                    elif matrix[m][mid] > target:
+                        r -= 1
+                    else: 
+                        return True
+            m += 1
+        return False
